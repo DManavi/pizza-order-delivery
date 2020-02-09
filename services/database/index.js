bec3645c77b07d4a44d5e1ca26ca2bd35a8e4a81
Danial Manavi <dmanavi@live.com>
2020-02-09 22:42:32 +0330

    Database initialization code.
    Changes to be committed:
    	modified:   services/database/index.js

--- services/database/index.js
+++ services/database/index.js
@@ -4,6 +4,21 @@
 
 'use strict';
 
-module.exports.create = ({ }) => {
-    return {};
-};
+const ms = require('ms');
+const { Sequelize } = require('sequelize');
+
+module.exports.create = ({ host, port, name, username, password, maximumClients, idleTimeout }) =>
+    new Sequelize(
+        {
+            dialect: 'postgres',
+            host,
+            port,
+            database: name,
+            username,
+            password,
+            pool: {
+                idle: ms(idleTimeout),
+                max: maximumClients
+            }
+        }
+    );
